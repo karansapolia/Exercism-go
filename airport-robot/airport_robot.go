4f6afa66d30f51ed50c4701a295bc88d0a08089e
Karan <10175625+karansapolia@users.noreply.github.com>
2025-06-20 19:05:32 +0100

    feat: add Italian and Portuguese structs

--- airport-robot/airport_robot.go
+++ airport-robot/airport_robot.go
@@ -10,6 +10,26 @@ type Greeter interface {
 	Greet(name string) string
 }
 
+type Italian struct{}
+
+func (i Italian) LanguageName() string {
+	return "Italian"
+}
+
+func (i Italian) Greet(name string) string {
+	return fmt.Sprintf("Ciao %s!", name)
+}
+
+type Portuguese struct{}
+
+func (p Portuguese) LanguageName() string {
+	return "Portuguese"
+}
+
+func (p Portuguese) Greet(name string) string {
+	return fmt.Sprintf("Olá %s!", name)
+}
+
 func SayHello(name string, implementer Greeter) string {
 	return fmt.Sprintf("I can speak %s: %s", implementer.LanguageName(), implementer.Greet(name))
 }
