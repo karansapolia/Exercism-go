1a9b071817bf51353e50c142920225e2881a9419
Karan <10175625+karansapolia@users.noreply.github.com>
2025-06-22 15:14:05 +0200

    feat: logs-logs-logs - complete WithinLimit fn.

--- logs-logs-logs/logs_logs_logs.go
+++ logs-logs-logs/logs_logs_logs.go
@@ -1,5 +1,7 @@
 package logs
 
+import "unicode/utf8"
+
 // Application identifies the application emitting the given log.
 func Application(log string) string {
 	for _, value := range log {
@@ -29,5 +31,5 @@ func Replace(log string, oldRune, newRune rune) string {
 // WithinLimit determines whether or not the number of characters in log is
 // within the limit.
 func WithinLimit(log string, limit int) bool {
-	panic("Please implement the WithinLimit() function")
+	return utf8.RuneCountInString(log) <= limit
 }
